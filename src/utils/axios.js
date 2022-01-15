7208271f17ebfc68b4b2b9a6bfabbbad20799eb0
N-dike <nnadike.dike@gmail.com>
2022-01-15 06:18:19 +0100

    upd

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/clone-73f9a/us-central1/api'
+    baseURL: 'https://us-central1-clone-73f9a.cloudfunctions.net/api'
     // cloud fn url
 })
 
 export default instance
+
+//http://localhost:5001/clone-73f9a/us-central1/api
